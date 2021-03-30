95ff904680936555b14f969972fbe4f844a4eae3
YUSEIRENDER <36284917+YUSEIRENDER@users.noreply.github.com>
2021-03-30 17:49:43 -0300

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -2,17 +2,57 @@ public class Motor
 {
     
     String nome;
-    double pontencia;
-    boolean ligado;
-
+    double potencia;
+    boolean ligado;            
+    
     public Motor(String nome, double pontencia, boolean ligado) {
         this.nome = nome;
-        this.pontencia = pontencia;
+        this.potencia = pontencia;
         this.ligado = ligado;
-        
         ligado=false;
     }
     
+    public String getNome()
+    {
+        
+        return nome;
+        
+    }
+    
+    public void setNome(String nome)
+    {
+        
+        this.nome = nome;
+        
+    }
+    
+    public double getPotencia()
+    {
+        
+        return potencia;
+        
+    }        
+    
+    public void setPotencia(double pontencia)
+    {
+        
+        this.potencia = potencia;
+        
+    }
+    
+    public boolean getLigado()
+    {
+        
+        return ligado;
+        
+    }
+    
+    public boolean setLigado(boolean ligado)
+    {
+        
+        return this.ligado = ligado;
+        
+    }        
     
     public boolean Ligar()
     {
@@ -31,7 +71,7 @@ public class Motor
     public void mostrar()
     {
         
-        System.out.println("|O modelo do motor é: " +nome+ "| a potencia do motor é: " +pontencia+ "| e o estado do motor é: " +ligado);
+        System.out.println("|O modelo do motor é: " +nome+ "| a potencia do motor é: " +potencia+ "| e o estado do motor é: " +ligado);
         
     }        
 }
